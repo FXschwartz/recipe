34c6e40c7ad89fd7f00f0b140f0058e26438ee35
Preston <preston.schwartz505@gmail.com>
2020-10-14 17:23:39 -0500

    Added bottom nav bar & started on top button group filter for meal times.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'widgets/bottom_nav_bar.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  int _page = 0;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]); // Hides the Top bar
+    SystemChrome.setPreferredOrientations([
+      // Only vertical orientation
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,98 +30,130 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ClipRRect(
+        borderRadius: BorderRadius.circular(
+          20.0,
+        ),
+        child: Scaffold(
+          bottomNavigationBar: BottomNavBar(),
+          body: Container(
+            color: Colors.white,
+            child: Column(
+              children: [FoodCatButtonBar()],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FoodCatButtonBar extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _FoodCatButtonBarState createState() => _FoodCatButtonBarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _FoodCatButtonBarState extends State<FoodCatButtonBar> {
+  bool breakfastButtonActive = true;
+  bool lunchButtonActive = false;
+  bool dinnerButtonActive = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ButtonBar(
+          buttonTextTheme: ButtonTextTheme.normal,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    width: 0.5,
+                    color:
+                        breakfastButtonActive ? Colors.lightBlue : Colors.white,
+                  ),
+                ),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    dinnerButtonActive = false;
+                    lunchButtonActive = false;
+                    breakfastButtonActive = true;
+                  });
+                },
+                child: Text(
+                  'Breakfast',
+                  style: TextStyle(
+                    color:
+                        breakfastButtonActive ? Colors.lightBlue : Colors.grey,
+                    fontSize: breakfastButtonActive ? 15 : 13,
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1.0,
+                      color:
+                          lunchButtonActive ? Colors.lightBlue : Colors.white,
+                    ),
+                  ),
+                ),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        lunchButtonActive = true;
+                        breakfastButtonActive = false;
+                        dinnerButtonActive = false;
+                      });
+                    },
+                    child: Text(
+                      'Lunch',
+                      style: TextStyle(
+                        color:
+                            lunchButtonActive ? Colors.lightBlue : Colors.grey,
+                        fontSize: lunchButtonActive ? 15 : 13,
+                      ),
+                    ))),
+            Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 0.5,
+                      color:
+                          dinnerButtonActive ? Colors.lightBlue : Colors.white,
+                    ),
+                  ),
+                ),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        dinnerButtonActive = true;
+                        lunchButtonActive = false;
+                        breakfastButtonActive = false;
+                      });
+                    },
+                    child: Text(
+                      'Dinner',
+                      style: TextStyle(
+                        color:
+                            dinnerButtonActive ? Colors.lightBlue : Colors.grey,
+                        fontSize: dinnerButtonActive ? 15 : 13,
+                      ),
+                    ))),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
